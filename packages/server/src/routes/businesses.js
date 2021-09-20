0ddc473487d7e08792c7eb35f0ea70b726178069
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2021-09-19 22:06:41 -0600

    get business by id and add new business endpoints working

--- packages/server/src/routes/businesses.js
+++ packages/server/src/routes/businesses.js
@@ -2,8 +2,27 @@ import express from 'express'
 const router = express.Router()
 import { Business } from '../models'
 
-router.get('/', async (request, response) => {
-    console.log('Get businesses')
+router.get('/:businessId', async (req, res) => {
+    const business = await Business.findById(req.params.businessId)
+
+    if (business) {
+        res.json(business.toJSON())
+    } else {
+        res.status(404).end()
+    }
+})
+
+router.post('/', async (req, res) => {
+    const business = new Business({
+        businessName: req.body.name
+    })
+    console.log(business)
+    try {
+        const newBusiness = await business.save()
+        res.status(200).json(newBusiness.toJSON())
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
 })
 
 module.exports = router
