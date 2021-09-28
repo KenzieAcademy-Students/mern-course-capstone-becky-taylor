38172b55dc56fa3f12a07be6b092486dc5b4cdcd
Becky Taylor <dev.becky.taylor@gmail.com>
2021-09-28 12:45:41 -0500

    Added ProductsList component

---
+++ packages/client/src/components/ProductsList/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProductsList.css'
+
+function ProductsList({ }) {
+  return (
+    <div>
+      Hello from Products List
+    </div>
+  )
+}
+
+export default ProductsList
