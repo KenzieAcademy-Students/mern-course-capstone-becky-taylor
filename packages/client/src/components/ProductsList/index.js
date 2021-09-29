97d96969b5b137b1f99ab6773ee5346dea245b2a
Becky Taylor <dev.becky.taylor@gmail.com>
2021-09-29 15:16:41 -0500

    Started adding config for axios

--- packages/client/src/components/ProductsList/index.js
+++ packages/client/src/components/ProductsList/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ProductsList.css'
+import { useApiFetch } from "util/api"
 import ProductDetails from 'components/ProductDetails'
 
 function ProductsList({ }) {
+  const {error, response} = useApiFetch("/products", {method: "GET"})
+  const {products, setProducts} = useState([])
+  /*useEffect(() => {
+      
+      try {
+        const res = useApiFetch("/products", {method: "GET"})
+        setProducts(res)
+        
+      } catch (error) {
+        console.log(error)
+      }
+   
+  }, []);*/
+  
+
   return (
     <div>
       Hello from Products List
+      { error && <h3 style={{color:"red"}}>Error Loading Data: {error}</h3>}
+      { !error && response && (
+        <div>Products: {response}</div>
+      )}
       <ProductDetails />
     </div>
   )
