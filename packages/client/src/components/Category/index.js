961d45a0de8acb148adcc92fea3fd619f3b86ef6
Becky Taylor <dev.becky.taylor@gmail.com>
2021-10-06 16:45:27 -0500

    starting to add category component

---
+++ packages/client/src/components/Category/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ListGroup, Image, Card, Button } from 'react-bootstrap'
+import './Category.css'
+import ProductDetails from 'components/ProductDetails'
+
+/*
+ Need to build out the Category component to:
+  -take in a category and it's corresponding list of products 
+  -call productsList component, passing in products
+ */
+
+function Category({ product, handleShowEditModal, handleShowDelModal, loggedIn }) {
+
+  return (
+    <div>
+     hello from category
+    </div>
+  )
+}
+
+export default Category
