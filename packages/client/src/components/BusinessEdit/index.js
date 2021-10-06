a496f2a8a8d939227f634f9e3831ec8212796300
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2021-10-06 15:31:17 -0600

    got business edit modal working

--- packages/client/src/components/BusinessEdit/index.js
+++ packages/client/src/components/BusinessEdit/index.js
@@ -1,26 +1,61 @@
 import React, { useEffect, useState } from 'react'
+import { Form, Button, Modal } from 'react-bootstrap'
 import './BusinessEdit.css'
 import axios from 'util/axiosConfig.js'
 
 function BusinessEdit({ }) {
+  const [isNewBusiness, setIsNewBusiness] = useState(false)
   const [business, setBusiness] = useState({})
+  const [formSubmit, setFormSubmit] = useState(false)
+  const [editing, setEditing] = useState(false)
+
+  const handleClose = () => setEditing(false);
+  const handleShow = () => setEditing(true);
 
   useEffect(() => {
-    async function getBusinessInfo() {
+    async function sendNewBusinessInfo() {
       try {
-        const businessInfo = await axios.get('/businesses/6143810dc4773662ace01286')
-        setBusiness(businessInfo.data)
+        // axios post/put request depending on isNewBusiness state
       } catch (err) {
         console.error(err.message)
       }
     }
-    getBusinessInfo()
-  }, [])
+    sendNewBusinessInfo()
+  }, [formSubmit])
 
   return (
-    <div id="business-edit">
-        
-    </div>
+    <>
+      <Button variant="success" onClick={handleShow}>Edit</Button>
+      <Modal show={editing} onHide={handleClose}>
+        <Form id="business-form">
+            <Form.Group>
+                <Form.Label>Business Name</Form.Label>
+                <Form.Control placeholder="Business Name" value={business.businessName}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Description</Form.Label>
+                <Form.Control placeholder="Description" value={business.businessDescription}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Address Line 1</Form.Label>
+                <Form.Control placeholder="Address Line 1" value={business.address1}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Address Line 2</Form.Label>
+                <Form.Control placeholder="Address Line 2" value={business.address2}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder="City" value={business.city}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Zip</Form.Label>
+                <Form.Control placeholder="Zip" value={business.stateZip}></Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit">Submit</Button>
+        </Form>
+      </Modal>
+    </>
   )
 }
 
--- packages/client/src/components/BusinessInfo/index.js
+++ packages/client/src/components/BusinessInfo/index.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { Card } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import './BusinessInfo.css'
 import axios from 'util/axiosConfig.js'
 import BusinessEdit from 'components/BusinessEdit'
 
 function BusinessInfo({ }) {
   const [business, setBusiness] = useState({})
+  const [refresh, setRefresh] = useState(false)
+
+  async function handleBusinessEdit() {
+    setRefresh(!refresh)
+  }
 
   useEffect(() => {
     async function getBusinessInfo() {
@@ -17,17 +22,21 @@ function BusinessInfo({ }) {
       }
     }
     getBusinessInfo()
-  }, [])
+  }, [refresh])
 
   return (
-    <div id="business-info">
-        <img src="https://www.mapbusinessonline.com/blog/wp-content/uploads/2021/05/earth_location_256x256.png" height="150px" width="150px" />
-        <div id="business-text">
-          <div id="business-name">{business.businessName}</div>
-          <div>{business.businessDescription}Placeholder description text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo erat et mi convallis, et suscipit massa porta. In ut ligula vel nunc scelerisque faucibus et eu nibh. Aliquam a gravida nisl, ac vulputate ligula. Integer et metus in urna eleifend feugiat varius non tellus. Fusce mattis eros quam, id ultrices justo feugiat vel. Nullam dapibus ornare porta. Donec ut posuere mi. In nec urna sit amet nunc vehicula accumsan in et lectus. Pellentesque tempus pulvinar nunc, vitae malesuada enim congue non. Nunc bibendum sem porta, varius ex sed, commodo elit. Maecenas vulputate sollicitudin erat volutpat finibus. Aenean eu lacus vel felis euismod luctus et eu ante. Aenean feugiat eleifend risus, id consequat justo tempor ut. Maecenas dignissim imperdiet luctus. Nulla ac velit ullamcorper, posuere nisl id, facilisis dolor. Morbi eu massa porta, rutrum libero ut, elementum sapien. Quisque erat magna, faucibus vitae ligula in, pellentesque aliquet erat. Morbi molestie ante quis nisl hendrerit hendrerit. Maecenas non auctor magna.</div>
-        </div>
-        <BusinessEdit />
-    </div>
+    <>
+      <div id="business-info">
+          <img src="https://www.mapbusinessonline.com/blog/wp-content/uploads/2021/05/earth_location_256x256.png" height="150px" width="150px" />
+          <div id="business-text">
+            <div id="business-name">{business.businessName}</div>
+            <div id="business-desc">{business.businessDescription}Placeholder description text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo erat et mi convallis, et suscipit massa porta. In ut ligula vel nunc scelerisque faucibus et eu nibh. Aliquam a gravida nisl, ac vulputate ligula. Integer et metus in urna eleifend feugiat varius non tellus. Fusce mattis eros quam, id ultrices justo feugiat vel. Nullam dapibus ornare porta. Donec ut posuere mi. In nec urna sit amet nunc vehicula accumsan in et lectus. Pellentesque tempus pulvinar nunc, vitae malesuada enim congue non. Nunc bibendum sem porta, varius ex sed, commodo elit. Maecenas vulputate sollicitudin erat volutpat finibus. Aenean eu lacus vel felis euismod luctus et eu ante. Aenean feugiat eleifend risus, id consequat justo tempor ut. Maecenas dignissim imperdiet luctus. Nulla ac velit ullamcorper, posuere nisl id, facilisis dolor. Morbi eu massa porta, rutrum libero ut, elementum sapien. Quisque erat magna, faucibus vitae ligula in, pellentesque aliquet erat. Morbi molestie ante quis nisl hendrerit hendrerit. Maecenas non auctor magna.</div>
+          </div>
+          <div>
+          <BusinessEdit />
+          </div>
+      </div>
+    </>
   )
 }
 
