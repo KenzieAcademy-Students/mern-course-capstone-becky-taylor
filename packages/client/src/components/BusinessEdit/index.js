7a7bfdfddbdee35ebe357b3e4e2a116fd68d263f
Ettb <toneyerrol@yahoo.com>
2021-10-08 16:21:07 -0400

    stylesOne

--- packages/client/src/components/BusinessEdit/index.js
+++ packages/client/src/components/BusinessEdit/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Form, Button, Modal } from 'react-bootstrap'
+import { Form, Button, Modal, Container } from 'react-bootstrap'
 import './BusinessEdit.css'
 import axios from 'util/axiosConfig.js'
 
@@ -24,38 +24,41 @@ function BusinessEdit({ }) {
   }, [formSubmit])
 
   return (
-    <>
-      <Button variant="success" onClick={handleShow}>Edit</Button>
+    <Container className="form-Container">
+      <Button variant="success" onClick={handleShow} id="edtBtn">Edit</Button>
       <Modal show={editing} onHide={handleClose}>
         <Form id="business-form">
-            <Form.Group>
+            <Form.Group className="formGroup">
                 <Form.Label>Business Name</Form.Label>
                 <Form.Control placeholder="Business Name" value={business.businessName}></Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group className="formGroup">
                 <Form.Label>Description</Form.Label>
                 <Form.Control placeholder="Description" value={business.businessDescription}></Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group className="formGroup">
                 <Form.Label>Address Line 1</Form.Label>
                 <Form.Control placeholder="Address Line 1" value={business.address1}></Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group className="formGroup">
                 <Form.Label>Address Line 2</Form.Label>
                 <Form.Control placeholder="Address Line 2" value={business.address2}></Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group className="formGroup">
                 <Form.Label>City</Form.Label>
                 <Form.Control placeholder="City" value={business.city}></Form.Control>
             </Form.Group>
-            <Form.Group>
+            <Form.Group className="formGroup">
                 <Form.Label>Zip</Form.Label>
                 <Form.Control placeholder="Zip" value={business.stateZip}></Form.Control>
             </Form.Group>
-            <Button variant="primary" type="submit">Submit</Button>
+           
+            <Button variant="primary" type="submit" id="sbMt-Btn">Submit</Button>
+          
+
         </Form>
       </Modal>
-    </>
+    </Container>
   )
 }
 
--- packages/client/src/components/BusinessInfo/index.js
+++ packages/client/src/components/BusinessInfo/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import { Container, Image, Row, Col } from 'react-bootstrap'
 import './BusinessInfo.css'
 import axios from 'util/axiosConfig.js'
 import BusinessEdit from 'components/BusinessEdit'
@@ -25,18 +25,21 @@ function BusinessInfo({ }) {
   }, [refresh])
 
   return (
-    <>
-      <div id="business-info">
-          <img src="https://www.mapbusinessonline.com/blog/wp-content/uploads/2021/05/earth_location_256x256.png" height="150px" width="150px" />
-          <div id="business-text">
-            <div id="business-name">{business.businessName}</div>
-            <div id="business-desc">{business.businessDescription}Placeholder description text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo erat et mi convallis, et suscipit massa porta. In ut ligula vel nunc scelerisque faucibus et eu nibh. Aliquam a gravida nisl, ac vulputate ligula. Integer et metus in urna eleifend feugiat varius non tellus. Fusce mattis eros quam, id ultrices justo feugiat vel. Nullam dapibus ornare porta. Donec ut posuere mi. In nec urna sit amet nunc vehicula accumsan in et lectus. Pellentesque tempus pulvinar nunc, vitae malesuada enim congue non. Nunc bibendum sem porta, varius ex sed, commodo elit. Maecenas vulputate sollicitudin erat volutpat finibus. Aenean eu lacus vel felis euismod luctus et eu ante. Aenean feugiat eleifend risus, id consequat justo tempor ut. Maecenas dignissim imperdiet luctus. Nulla ac velit ullamcorper, posuere nisl id, facilisis dolor. Morbi eu massa porta, rutrum libero ut, elementum sapien. Quisque erat magna, faucibus vitae ligula in, pellentesque aliquet erat. Morbi molestie ante quis nisl hendrerit hendrerit. Maecenas non auctor magna.</div>
-          </div>
+    <Container className="box">
+        
+      <Row id="business-info">
+      <Col sm={true}>
+          <Image src="https://www.mapbusinessonline.com/blog/wp-content/uploads/2021/05/earth_location_256x256.png" height="150px" width="150px" id="imageGlobe" rounded />
+     </Col>
+          <Row  id="business-text">
+            <Col sm={true} id="business-name">businessPlaceHolderName{business.businessName}</Col>
+            <Col sm={true} id="business-desc">{business.businessDescription}Placeholder description text: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo erat et mi convallis, et suscipit massa porta. In ut ligula vel nunc scelerisque faucibus et eu nibh. Aliquam a gravida nisl, ac vulputate ligula. Integer et metus in urna eleifend feugiat varius non tellus. Fusce mattis eros quam, id ultrices justo feugiat vel. Nullam dapibus ornare porta. Donec ut posuere mi. In nec urna sit amet nunc vehicula accumsan in et lectus. Pellentesque tempus pulvinar nunc, vitae malesuada enim congue non. Nunc bibendum sem porta, varius ex sed, commodo elit. Maecenas vulputate sollicitudin erat volutpat finibus. Aenean eu lacus vel felis euismod luctus et eu ante. Aenean feugiat eleifend risus, id consequat justo tempor ut. Maecenas dignissim imperdiet luctus. Nulla ac velit ullamcorper, posuere nisl id, facilisis dolor. Morbi eu massa porta, rutrum libero ut, elementum sapien. Quisque erat magna, faucibus vitae ligula in, pellentesque aliquet erat. Morbi molestie ante quis nisl hendrerit hendrerit. Maecenas non auctor magna.</Col>
+          </Row>
           <div>
           <BusinessEdit />
           </div>
-      </div>
-    </>
+      </Row>
+    </Container>
   )
 }
 
