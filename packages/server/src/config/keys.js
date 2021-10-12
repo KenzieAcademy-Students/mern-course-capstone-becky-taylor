32d4ace1f93e46daa94b696a060cdddd718d6a92
Becky Taylor <dev.becky.taylor@gmail.com>
2021-10-12 15:25:20 -0500

    changed db name to try to fix error

--- packages/server/src/config/keys.js
+++ packages/server/src/config/keys.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   database: {
     // url: process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNApp', // for local mongodb
-    url: "mongodb+srv://bongo-admin:Y0uC@nD0It@cluster0.jb6ow.mongodb.net/bongo?retryWrites=true&w=majority" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
+    url: "mongodb+srv://bongo-admin:Y0uC@nD0It@cluster0.jb6ow.mongodb.net/Cluster0?retryWrites=true&w=majority" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'jwt-secret',
