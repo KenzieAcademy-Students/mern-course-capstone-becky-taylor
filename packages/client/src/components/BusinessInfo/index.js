a7e84b8fabf4833f957344b24ab8f99e6ca95a80
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2021-10-14 14:40:33 -0600

    added logo functionality to business edit

--- packages/client/src/components/BusinessInfo/index.js
+++ packages/client/src/components/BusinessInfo/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Image, Row, Col, Button, Modal } from 'react-bootstrap'
 import './BusinessInfo.css'
-import axios from 'util/axiosConfig.js'
 import BusinessEdit from 'components/BusinessEdit'
 
 function BusinessInfo({ businessObj, handleBusinessChange, loggedIn }) {
@@ -13,7 +12,7 @@ function BusinessInfo({ businessObj, handleBusinessChange, loggedIn }) {
   return (
     <>
       <div id="business-info">
-          <img src="https://www.mapbusinessonline.com/blog/wp-content/uploads/2021/05/earth_location_256x256.png" height="150px" width="150px" />
+          <img src={businessObj.logo} height="150px" width="150px" />
           <div id="business-text">
             <div id="business-name">{businessObj.businessName}</div>
             <div id="business-desc">{businessObj.businessDescription}</div>
