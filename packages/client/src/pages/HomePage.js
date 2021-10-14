c21c9957fc82158379ce914c18d4d00f3c04218d
Becky Taylor <dev.becky.taylor@gmail.com>
2021-10-14 16:04:47 -0500

    fixing a minor bug

--- packages/client/src/pages/HomePage.js
+++ packages/client/src/pages/HomePage.js
@@ -64,7 +64,7 @@ export default function HomePage(props) {
         </>
       ) : (
         <>
-          {businessRegistered && (
+          {!businessRegistered && (
           <>
           <Button onClick={() => setShowRegisterBusinessModal(true)} >Register Your Business</Button> &nbsp;
           </>
