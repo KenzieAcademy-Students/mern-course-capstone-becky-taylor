792f8c02a6e56319acdf7585ba86f297a74af33d
Becky Taylor <dev.becky.taylor@gmail.com>
2021-10-15 08:35:09 -0500

    Changed product details around and added css to make it more responsive

--- packages/client/src/components/ProductDetails/index.js
+++ packages/client/src/components/ProductDetails/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ListGroup, Image, Card, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import './ProductDetails.css'
 
 function ProductDetails({ product, handleShowEditModal, handleShowDelModal, loggedIn }) {
@@ -13,42 +13,25 @@ function ProductDetails({ product, handleShowEditModal, handleShowDelModal, logg
   }
 
   return (
-    <div>
-     <ListGroup horizontal>
-      { product.image && 
-      <ListGroup.Item><Image src={product.image} style={{width: '150px'}} /></ListGroup.Item>
-      }
-      <ListGroup.Item>
-      <Card style={{ width: '40rem', border: '0px' }}>
-        <Card.Body>
-          <Card.Title>{product.productName}</Card.Title>
-          <Card.Text>
-          {product.description}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      </ListGroup.Item>
-      <ListGroup.Item>
-      <Card style={{ width: '10rem', border: '0px' }}>
-        <Card.Body>
-          <Card.Title>${product.price.toFixed(2)}</Card.Title>
+    <div class="product-info">
+        { product.image && 
+          <img src={product.image} height="150px" width="150px" />
+        }
           
-            {loggedIn && (
+        <div class="product-text">
+          <div class="product-name">{product.productName}</div>
+          <div class="product-desc">{product.description}</div>
+        </div>
+        <div class="product-price">${product.price.toFixed(2)}
+          {loggedIn && (
             <div>
-            <Button variant="success" 
-              onClick={() => handleEditClick(product)} 
-              key={`edit_${product._id}`}>Edit</Button> 
-            <Button variant="danger" 
-              onClick={() => handleDeleteClick(product)} 
-              key={`del_${product._id}`}>X</Button>
-              </div>)
-            }
-          
-        </Card.Body>
-      </Card>
-      </ListGroup.Item>
-     </ListGroup>
+              <Button variant="success" onClick={() => handleEditClick(product)} key={`edit_${product._id}`}>Edit</Button> 
+              <Button variant="danger" onClick={() => handleDeleteClick(product)} key={`del_${product._id}`}>X</Button>
+            </div>
+          )}
+        </div>
     </div>
+  
   )
 }
 
